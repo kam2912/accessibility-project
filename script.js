eab8f3b396fc826902bda6c622306885cfcfadb8
Louise <lgilligang@gmail.com>
2019-11-27 20:24:49 +0000

    Thoughts on Javascript logic
    My thoughts on how we can make objects/functions - please look and amend

---
+++ script.js
@@ -0,0 +1,50 @@
+//var canvas = document.querySelector(".game"); - Game is e.g. class id from html 
+//canvas.width = tbc;
+//canvas.height = tbc;
+//var ctx = canvas.getContext("2d");
+
+
+// all figures/objects will go in the state
+
+// var state = {
+//   playerone:{
+//     x: 250,
+//     y: 250,
+//   },
+//   playertwo: {
+//     x:,
+//     y:,
+//   }
+// }
+
+
+
+// I was thinking we could have a function that moves the player up and down past obstacles.
+//That means we can build in an object detection warning if they collide with something e.g.:
+
+//function movePlayer (e){
+//  state.playerone.y = state.playerone.y - 100;
+
+//} canvas.addEventListener("click", movePlayer)
+
+
+
+
+
+
+
+//Then we need all the functions to move things on to the page.
+
+//There will be 1 function to run all the functions
+
+// 1 clear canvas function
+
+//make playerone function
+
+//make flashing light functions
+
+// make people getting in the way function
+
+// make confusing signs function
+
+// make clock ticking function
